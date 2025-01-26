34b0a05997b95c2a2f4f46fe299c2b559e3a535d
Vivid-Vortex <ctntc9.dpk@gmail.com>
2025-01-26 12:53:09 +0530

    NullEmpty Source handling strategy

---
+++ MapStructDemo/src/main/java/com/demo/mapstruct/MapStructDemo/mapper/NullEmptySourceCheckStrategy.java
@@ -0,0 +1,25 @@
+package com.demo.mapstruct.MapStructDemo.mapper;
+
+import com.demo.mapstruct.MapStructDemo.source.dto.SourceUserLocation;
+import com.demo.mapstruct.MapStructDemo.target.dto.TargetUserLocation;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+
+/**
+ * if any source is null, then explore:
+ *
+ * Use either of below 2 options beside target:
+ * 1. if source is null, then you can give default value to replace null
+ * 2. nullValueCheckStrategy = NulVAlueCheckSttrategy.ALWAYS
+ *
+ * if any source is empty, then explore:
+ *
+ * @BeanMapping(ignoreByDefault = true) // Ignore all unmapped properties by default and only map those getting explicitly mapped here.
+ */
+public interface NullEmptySourceCheckStrategy {
+    @Mapping(source = "phoneNumber", target = "mobile", defaultValue = "1234567890") // If phoneNumber is null, then default value will be used.
+    @Mapping(source = "email", target = "contactEmail", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+    @BeanMapping(ignoreByDefault = true) // Ignore all unmapped properties by default and only map those getting explicitly mapped here.
+    TargetUserLocation sourceToTarget(SourceUserLocation source);
+}
